9d1c4bce6ee88ed7951da103c1a7b442b05ee123
Yury Rivera <yrivera@applaudostudios.com>
2022-10-06 12:27:10 -0600

    extract stuff

--- VBA.Compiler/Package/StagePack.cs
+++ VBA.Compiler/Package/StagePack.cs
@@ -1,22 +1,39 @@
-﻿namespace VBA.Compiler.Package;
+﻿using System.IO.Compression;
+
+namespace VBA.Compiler.Package;
 
 public class StagePack
 {
+    private const string StageName = "vba-xlr8";
+
     public void CopyXmlFiles()
     {
         string tempDir = Path.GetTempPath();
         string cwd = Directory.GetCurrentDirectory();
         string pathName = cwd.Split('\\')[^1];
         Console.WriteLine(pathName);
-        string tempPath = Path.Join(tempDir, pathName);
+        string tempPath = Path.Join(tempDir, StageName, pathName, "src");
         Directory.CreateDirectory(tempPath);
+        Directory.Delete(Path.Combine(tempDir, StageName), true);
         CopyDir("_rels", tempPath);
         CopyDir("docProps", tempPath);
         CopyDir("xl", tempPath);
         File.Copy("[Content_Types].xml", Path.Combine(tempPath, "[Content_Types].xml"), true);
     }
 
-    private void CopyDir(string path, string temp)
+    public string Compress()
+    {
+        string tempDir = Path.GetTempPath();
+        string cwd = Directory.GetCurrentDirectory();
+        string pathName = cwd.Split('\\')[^1];
+        string tempPath = Path.Join(tempDir, StageName, pathName, "src");
+        string filePath = Path.Join(tempDir, StageName, pathName, $"{pathName}.xlsm");
+        Console.WriteLine($"Compressing {filePath}");
+        ZipFile.CreateFromDirectory(tempPath, filePath, CompressionLevel.NoCompression, false);
+        return filePath;
+    }
+
+    private static void CopyDir(string path, string temp)
     {
         string currentTemp = Path.Combine(temp, path);
         Directory.CreateDirectory(currentTemp);
@@ -24,7 +41,12 @@ public class StagePack
         {
             string fName = Path.GetFileName(filePath);
             Console.WriteLine("Copying {0} {1}", currentTemp, fName);
-            File.Copy(filePath, Path.Combine(currentTemp, fName));
+            File.Copy(filePath, Path.Combine(currentTemp, fName), true);
+        }
+
+        foreach (string directory in Directory.GetDirectories(path))
+        {
+            CopyDir(directory, temp);
         }
     }
 }
--- VBA.Compiler/Program.cs
+++ VBA.Compiler/Program.cs
@@ -1,4 +1,7 @@
-﻿using VBA.Compiler.Package;
+﻿using System.Diagnostics;
+using OfficeOpenXml;
+using OfficeOpenXml.VBA;
+using VBA.Compiler.Package;
 using VBA.Compiler.Responses;
 
 namespace VBA.Compiler;
@@ -7,30 +10,86 @@ public static class Program
 {
     public static int Main(string[] args)
     {
-        bool hasConfig = File.Exists("vba.json");
-        if (!hasConfig)
+        try
         {
-            NodeOutput.SendError(new AppError
+            if (args[0] == "e")
             {
-                Message = $"No vba.json file found in current folder",
-            });
-            return 0;
+                var info2 = new FileInfo(args[1]);
+                var package2 = new ExcelPackage(info2);
+                var vba = package2.Workbook.VbaProject;
+                foreach (var constant in vba.Constants)
+                {
+                    Console.WriteLine(constant);
+                }
+                foreach (var reference in vba.References)
+                {
+                    Console.WriteLine($"{reference.Libid}-{reference.Name}");
+                }
+
+                Directory.CreateDirectory("Vshit");
+                Directory.CreateDirectory("unknown");
+                foreach (var module in vba.Modules)
+                {
+                    Console.WriteLine($"{module.Name}:");
+                    foreach (var attribute in module.Attributes)
+                    {
+                        Console.Write($"{attribute.Name}={attribute.Value}");
+                    }
+
+                    string folder = module.Type switch
+                    {
+                        eModuleType.Document => "Vobjects",
+                        eModuleType.Module => "Vmodules",
+                        eModuleType.Class => "Vclasses",
+                        eModuleType.Designer => "Vshit",
+                        _ => "unknown"
+                    };
+                    using var ff = File.CreateText(Path.Join(folder, $"{module.Name}.vba"));
+                    ff.Write(module.Code);
+                    ff.Flush();
+                    ff.Close();
+                }
+                return 0;
+            }
+            bool hasConfig = File.Exists("vba.json");
+            if (!hasConfig)
+            {
+                NodeOutput.SendError(new AppError
+                {
+                    Message = $"No vba.json file found in current folder",
+                });
+                return 0;
+            }
+
+            var packager = new StagePack();
+            packager.CopyXmlFiles();
+
+            string path = packager.Compress();
+            var info = new FileInfo(path);
+            var package = new ExcelPackage(info);
+            ExcelWorksheet? sheet = package.Workbook.Worksheets["Aurora"];
+            sheet.Cells[7, 1].Value = $"current time {DateTime.Now:hh:mm:ss}";
+            package.Save();
+            var startInfo = new ProcessStartInfo(@"C:\Program Files\Microsoft Office\Office15\EXCEL.EXE")
+            {
+                WindowStyle = ProcessWindowStyle.Normal,
+                Arguments = path,
+            };
+            Process proc = Process.Start(startInfo);
+            proc.WaitForExit();
+
+            // StreamReader stream = File.OpenText("Objects/Aurora.vba");
+            // string vbaSource = stream.ReadToEnd();
+            //
+            // package.Workbook.CreateVBAProject();
+            // var project = 
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.Message);
+            Console.WriteLine(e);
         }
 
-        var packager = new StagePack();
-        packager.CopyXmlFiles();
-        const string path = "sample_data.xlsx";
-        // var fileInfo = new FileInfo(path);
-        // var package = new ExcelPackage();
-        // ExcelWorksheet? sheet = package.Workbook.Worksheets.Add("test");
-        // sheet.SetValue("A1", 69);
-        // Thread.Sleep(1000);
-        //
-        // StreamReader stream = File.OpenText("Objects/Aurora.vba");
-        // string vbaSource = stream.ReadToEnd();
-        //
-        // package.Workbook.CreateVBAProject();
-        // var project = 
         return 0;
     }
 }
